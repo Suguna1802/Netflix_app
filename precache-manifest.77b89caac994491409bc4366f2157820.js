27afa9683ae6dee69be11523e87148ea3bf9d6e2
suguna <sugunamc0@gmail.com>
2022-05-06 15:57:25 +0530

    Updates

---
+++ precache-manifest.77b89caac994491409bc4366f2157820.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "717cf0df521b7c971ecce3a4690b2b01",
+    "url": "/Netflix_app/index.html"
+  },
+  {
+    "revision": "5922e605209e70090eab",
+    "url": "/Netflix_app/static/css/main.c4fe4ee9.chunk.css"
+  },
+  {
+    "revision": "0346f0130303eb8d1388",
+    "url": "/Netflix_app/static/js/2.2bc7a627.chunk.js"
+  },
+  {
+    "revision": "8c531855ea370064c601b9c584684a5a",
+    "url": "/Netflix_app/static/js/2.2bc7a627.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5922e605209e70090eab",
+    "url": "/Netflix_app/static/js/main.4a61f9ca.chunk.js"
+  },
+  {
+    "revision": "3d0033ed73d3db7e9594",
+    "url": "/Netflix_app/static/js/runtime-main.703a5df8.js"
+  }
+]);
